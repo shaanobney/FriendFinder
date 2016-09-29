8dd5bc6d1bce33bf558bc6c7f259d6c9a3e5d0ea
shaanobney <shaan.obney@gmail.com>
2016-09-29 12:36:27 -0700

    added dummy data

--- app/routing/html-routes.js
+++ app/routing/html-routes.js
@@ -7,9 +7,6 @@ module.exports = function(app){
 	app.get('/survey', function(req, res){
 		res.sendFile(path.join(__dirname + '/../public/survey.html'));
 	})
-	app.get('/profile', function(req, res){
-		res.sendFile(path.join(__dirname + '/../public/profile.html'))
-	})
 	app.use(function(req, res){
 		res.sendFile(path.join(__dirname + '/../public/home.html'));
 	})
