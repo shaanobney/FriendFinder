e295b3702d7d7e82a547843a43440e81f341c5c5
shaanobney <shaan.obney@gmail.com>
2016-09-29 16:04:24 -0700

    fixed layout

--- app/routing/api-routes.js
+++ app/routing/api-routes.js
@@ -1,16 +1,16 @@
 var friends = require('../data/friends.js');
 var path = require('path');
 
-module.exports = function(app){
-	app.get('/api/friends', function(req, res){
+module.exports = function(app) {
+	app.get('/api/friends', function(req, res) {
 		res.json(friends);
 	});
-	app.post('/api/friends', function(req, res){
+	app.post('/api/friends', function(req, res) {
 		var match;
 		var least = 10000;
-		for(var i=0; i<friends.length; i++){
+		for(var i=0; i<friends.length; i++) {
 			var newDif = 0;
-			for(var j=0; j<friends[i].scores.length; j++){
+			for(var j=0; j<friends[i].scores.length; j++) {
 				newDif += Math.abs(friends[i].scores[j] - req.body.scores[j])
 			};
 			if(newDif < least){
